b42f52888427104b09505195f3df438fd58158e8
faij1207 <faij12072000@gmail.com>
2024-09-18 11:02:26 +0530

    create search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -8,6 +8,7 @@ router.post('/create',verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id', verifyToken, updateListing);
 router.get('/get/:id', verifyToken, getListing);
+router.get('/get',  getListing);
 
 export default router;  
 
